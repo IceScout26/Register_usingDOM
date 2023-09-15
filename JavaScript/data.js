82bfcdfa259d06cf60aefe26e9c41098cd9f4b26
IceScout26 <dhimasscout@gmail.com>
2023-09-15 18:23:37 +0700

    feat : average, show data, total money

--- JavaScript/data.js
+++ JavaScript/data.js
@@ -1,3 +1,50 @@
+const registerList = []; // Array untuk menyimpan data register
+
+// Fungsi untuk menghitung rata-rata umur dan uang sangu
+function average() {
+    let totalAge = 0;
+    let totalMoney = 0;
+
+    for (const register of registerList) {
+        totalAge += register.age;
+        totalMoney += register.money;
+    }
+
+    const averageAge = totalAge / registerList.length;
+    const averageMoney = totalMoney / registerList.length;
+
+    return {
+        averageAge,
+        averageMoney
+    };
+}
+
+// Fungsi untuk menampilkan data register dan resume
+function tampilkanData() {
+    const resume = document.getElementById("resume");
+    const registerTable = document.getElementById("register-list");
+    const {
+        averageAge,
+        averageMoney
+    } = average();
+
+    // Tampilkan resume
+    resume.innerHTML = `Rata-rata register memiliki uang sangu sebesar ${averageMoney.toFixed(2)} dengan rata-rata umur ${averageAge.toFixed(2)}`;
+
+    // Tampilkan data register
+    registerTable.innerHTML = "";
+    for (const register of registerList) {
+        const row = `
+            <tr>
+                <td>${register.name}</td>
+                <td>${register.age}</td>
+                <td>${register.money}</td>
+            </tr>
+        `;
+        registerTable.innerHTML += row;
+    }
+}
+
 // Event listener for form submission
 document.getElementById("registration-form").addEventListener("submit", function (event) {
     event.preventDefault();
@@ -13,7 +60,11 @@ document.getElementById("registration-form").addEventListener("submit", function
     }
 
     // add data register to array
-    pendaftarList.push({ name, age, money });
+    registerList.push({
+        name,
+        age,
+        money
+    });
 
     // clear input
     document.getElementById("name").value = "";
